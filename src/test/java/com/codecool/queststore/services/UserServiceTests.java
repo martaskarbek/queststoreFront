4d9ce9a858f22556ba3d8ab0f1dd5d4c436fa3ed
Marta Skarbek <marta@skarbek.name>
2020-09-30 17:03:43 +0200

    refactored naming of users

--- src/test/java/com/codecool/queststore/services/UserServiceTests.java
+++ src/test/java/com/codecool/queststore/services/UserServiceTests.java
@@ -2,12 +2,7 @@ package com.codecool.queststore.services;
 
 import com.codecool.queststore.dao.SessionPostgreSQLDAO;
 import com.codecool.queststore.dao.UserPostgreSQLDAO;
-import com.codecool.queststore.models.Role;
 import com.codecool.queststore.models.Session;
-import com.codecool.queststore.models.users.Mentor;
-import com.codecool.queststore.models.users.User;
-import com.codecool.queststore.services.UserService;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
