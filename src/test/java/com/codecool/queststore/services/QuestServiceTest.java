5f3429f76e613975daf48ae4ee66d6b0deb731d5
Marta Skarbek <marta@skarbek.name>
2020-10-01 11:28:42 +0200

    added quest edit test

--- src/test/java/com/codecool/queststore/services/QuestServiceTest.java
+++ src/test/java/com/codecool/queststore/services/QuestServiceTest.java
@@ -1,28 +1,54 @@
 package com.codecool.queststore.services;
 
+import com.codecool.queststore.dao.QuestDAO;
 import com.codecool.queststore.models.Quest;
 import com.codecool.queststore.models.users.Mentor;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import java.util.HashMap;
 import java.util.Map;
 
 class QuestServiceTest {
+
+    QuestService questService = Mockito.mock(QuestService.class);
+    Map<String, String> questsMap = Mockito.mock(Map.class);
+    Mentor mentor = new Mentor();
 
     @Test
     void checkQuestUpdate() {
 
         //Arrange
-        Map<String, String> questsMap = new HashMap<>();
-        Mentor mentor = new Mentor();
-        QuestService questService = Mockito.mock(QuestService.class);
+        //Everything prepared up
 
         //Act
         questService.updateQuest(questsMap, mentor);
 
         //Assert
         Mockito.verify(questService).updateQuest(questsMap, mentor);
+    }
+
+    @Test
+    void checkCreateQuestModel() {
+        //Arrange
+        //Everything prepared up
+
+        //Act
+        questService.createQuest(questsMap, mentor);
+
+        //Assert
+        Mockito.verify(questService).createQuest(questsMap, mentor);
+    }
+
+    @Test
+    void checkUpdateQuest() {
+        //Arrange
+        Quest quest = new Quest();
+
+        //Act
+        questService.updateQuest(quest);
+
+        //Assert
+        Mockito.verify(questService).updateQuest(quest);
     }
 
 }
