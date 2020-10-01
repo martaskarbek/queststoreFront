3643333790638d716df3802f3d2fc2d051d113bb
Marta Skarbek <marta@skarbek.name>
2020-10-01 23:31:40 +0200

    refactored methods

--- src/test/java/com/codecool/queststore/services/UserServiceTests.java
+++ src/test/java/com/codecool/queststore/services/UserServiceTests.java
@@ -3,19 +3,19 @@ package com.codecool.queststore.services;
 import com.codecool.queststore.dao.SessionPostgreSQLDAO;
 import com.codecool.queststore.dao.UserPostgreSQLDAO;
 import com.codecool.queststore.models.Session;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 public class UserServiceTests {
 
+    Session session = new Session("9395885b-d27e-489f-8a3a-363aa7d93603", 1);
+    SessionPostgreSQLDAO sessionPostgreSQLDAO = Mockito.mock(SessionPostgreSQLDAO.class);
+
     @Test
     void checkUserSessionID() {
 
         //Arrange
-        Session session = new Session("9395885b-d27e-489f-8a3a-363aa7d93603", 1);
         UserPostgreSQLDAO userPostgreSQLDAO = Mockito.mock(UserPostgreSQLDAO.class);
-        SessionPostgreSQLDAO sessionPostgreSQLDAO = Mockito.mock(SessionPostgreSQLDAO.class);
         Mockito.when(sessionPostgreSQLDAO.getBySessionId(session.getUuid())).thenReturn(session);
         UserService userService = new UserService(userPostgreSQLDAO, sessionPostgreSQLDAO);
 
@@ -27,15 +27,16 @@ public class UserServiceTests {
     }
 
     @Test
-
     void checkLogout() {
-        Session session = new Session("9395885b-d27e-489f-8a3a-363aa7d93603", 1);
-        SessionPostgreSQLDAO sessionPostgreSQLDAO = Mockito.mock(SessionPostgreSQLDAO.class);
+
+        //Arrange
         Mockito.when(sessionPostgreSQLDAO.getBySessionId(session.getUuid())).thenReturn(session);
         UserService userService = new UserService(null, sessionPostgreSQLDAO);
 
+        //Act
         userService.logout(session.getUuid());
 
+        //Assert
         Mockito.verify(sessionPostgreSQLDAO).remove(session);
     }
 
