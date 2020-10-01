0a287a46af69a3b6ed7cfeabf8154eb94d2c0b46
Marta Skarbek <marta@skarbek.name>
2020-10-01 12:35:17 +0200

    added test for check is returning quest by id given

--- src/test/java/com/codecool/queststore/services/QuestServiceTest.java
+++ src/test/java/com/codecool/queststore/services/QuestServiceTest.java
@@ -13,6 +13,7 @@ class QuestServiceTest {
     QuestService questService = Mockito.mock(QuestService.class);
     Map<String, String> questsMap = Mockito.mock(Map.class);
     Mentor mentor = new Mentor();
+    Quest quest = new Quest();
 
     @Test
     void checkQuestUpdate() {
@@ -29,6 +30,7 @@ class QuestServiceTest {
 
     @Test
     void checkCreateQuestModel() {
+
         //Arrange
         //Everything prepared up
 
@@ -41,14 +43,43 @@ class QuestServiceTest {
 
     @Test
     void checkUpdateQuest() {
+
         //Arrange
-        Quest quest = new Quest();
+        //Everything prepared up
 
         //Act
         questService.updateQuest(quest);
 
         //Assert
         Mockito.verify(questService).updateQuest(quest);
+    }
+
+    @Test
+    void checkingAddQuest() {
+
+        //Arrange
+        //Everything prepared up
+
+        //Act
+        questService.addQuest(quest);
+
+        //Assert
+        Mockito.verify(questService).addQuest(quest);
+    }
+
+    @Test
+    void checkGetQuestById() {
+
+        //Arrange
+        int questId = 1;
+        QuestDAO questDAO = Mockito.mock(QuestDAO.class);
+        Mockito.when(questDAO.get(questId)).thenReturn(quest);
+
+        //Act
+        questService.getQuest(questId);
+
+        //Assert
+        Mockito.verify(questService).getQuest(questId);
     }
 
 }
