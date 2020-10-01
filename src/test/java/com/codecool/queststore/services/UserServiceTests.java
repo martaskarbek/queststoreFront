f0614195f1aad81bf87ad4f79becd5d745a8f9fc
Marta Skarbek <marta@skarbek.name>
2020-10-01 23:07:09 +0200

    added commit for logout function

--- src/test/java/com/codecool/queststore/services/UserServiceTests.java
+++ src/test/java/com/codecool/queststore/services/UserServiceTests.java
@@ -3,6 +3,7 @@ package com.codecool.queststore.services;
 import com.codecool.queststore.dao.SessionPostgreSQLDAO;
 import com.codecool.queststore.dao.UserPostgreSQLDAO;
 import com.codecool.queststore.models.Session;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
@@ -23,6 +24,19 @@ public class UserServiceTests {
 
         //Assert
        Mockito.verify(userPostgreSQLDAO).get(session.getUserId());
+    }
+
+    @Test
+
+    void checkLogout() {
+        Session session = new Session("9395885b-d27e-489f-8a3a-363aa7d93603", 1);
+        SessionPostgreSQLDAO sessionPostgreSQLDAO = Mockito.mock(SessionPostgreSQLDAO.class);
+        Mockito.when(sessionPostgreSQLDAO.getBySessionId(session.getUuid())).thenReturn(session);
+        UserService userService = new UserService(null, sessionPostgreSQLDAO);
+
+        userService.logout(session.getUuid());
+
+        Mockito.verify(sessionPostgreSQLDAO).remove(session);
     }
 
 }
